923b6252d396eaf98ca0548e7a4b1060b6512b31
杨万里 <1091231787@qq.com>
2020-10-23 15:37:26 +0800

    test

--- config.php
+++ config.php
@@ -1,3 +1,4 @@
 <?php
-$SIGNALING_ADDRESS = 'ws://127.0.0.1:9509';
+//$SIGNALING_ADDRESS = 'ws://127.0.0.1:9509';
+$SIGNALING_ADDRESS = 'ws://room.nutwisdom.cn:9509';
 //$STUN_ADDRESS = 'stun:stun.xten.com';
